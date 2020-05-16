47bc91dad22c64bfd7c607428c0877ac0385a870
Deok <ziinxed@gmail.com>
2020-05-16 22:04:40 +0900

    state and set state and current

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodIlike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image: "",
-    rating: 4.9,
-  },
-  {
-    id: 2,
-    name: "samgyeop",
-    image: "",
-    rating: 5.0,
-  },
-];
+//state is about data that changes -> dynamic data
 
 function Food({ name, image, rating }) {
   // props 바로 접근 instead of props.fav
@@ -35,21 +22,35 @@ Food.propTypes = {
   rating: PropTypes.number.isRequired,
 };
 
-function App() {
-  return (
-    <div className="App">
-      {foodIlike.map((dish) => (
-        <Food
-          key={dish.id} // react element unique -> react internal use
-          name={dish.name}
-          image={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-} // give prop fav to food in jsx
-
+class App extends React.Component {
+  state = {
+    // state is object -> data that will change
+    count: 0,
+  }; //do not mutate state directly
+  //mutating state should re render() how?
+  //using setState() it is possible to refresh render() function
+  add = () => {
+    console.log(`add`);
+    this.setState({ count: this.state.count + 1 }); // 추천되지 않는 방법이다.
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState((current) => ({
+      count: current.count - 1,
+    })); // recommended 함수 이름이 없는 화살표 함수로 한문장이기때문에 {}역시 생략했다.
+  };
+  render() {
+    return (
+      <div>
+        <h1>
+          The number is : {this.state.count} component
+        </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    ); //함수에 괄호가 붙으면 바로 실행 아니면 클릭시에만 실행
+  }
+}
 export default App;
 // javascript code go in {}
 //map takes function and execute for each item in array
