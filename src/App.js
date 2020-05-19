a4b183ce83c909e1bf597c7774da132b06a6a74e
Gi-Deok Jang <deok@Gi-Deokui-iMac.local>
2020-05-20 02:00:12 +0900

    CSS Study needed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
       <Route exact={true} path="/" component={Home} />
-      <Route path="/about">
-        <h1>About</h1>
-      </Route>
+      <Route path="/about" component={About} />
     </HashRouter>
   );
 }
