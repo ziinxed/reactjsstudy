669975e650e98da6d4d0901de3272628208648b4
Deok <ziinxed@gmail.com>
2020-05-22 23:03:27 +0900

    navigation and router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
-import "./App.css";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route exact={true} path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/" exact={true} component={Home} />
     </HashRouter>
   );
 }
